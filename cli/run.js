4ebc14d5cc08f131df81d00d7d042424e0513a26
Mora <qiuzhongleiabc@126.com>
2016-06-23 17:19:26 +0800

    feat(cli): add npm bin directory to run script

--- cli/run.js
+++ cli/run.js
@@ -1,17 +1,25 @@
 #!/usr/bin/env node
 
+var path = require('path');
 var spawn = require('cross-spawn');
 var assign = require('../libs/assign');
 var findup = require('../libs/findup');
 var dotProp = require('../libs/dot-prop');
+var addEnvPath = require('../libs/addEnvPath');
 var warn = require('../libs/warn');
 
-var pkg = require(findup.pkg());
+var pkgFile = findup.pkg();
+var rootDir = path.dirname(pkgFile);
+
+var pkg = require(pkgFile);
 var reEnvArg = /^(\w+)=(?:'(.+)'|"(.+)"|(.+))$/;
 var rePkgArg = /^pkg(?:\.[\w-]+)+$/;
 var rePkgArgs = /\bpkg(?:\.[\w-]+)+\b/g;
 
 var env = assign({}, process.env), cmd, args = [];
+
+addEnvPath(env, path.join(rootDir, 'node_modules', '.bin'));
+addEnvPath(env, path.join(rootDir, 'bin'));
 
 process.argv.slice(2).map(function (arg) {
   if (reEnvArg.test(arg)) {
