a531c14de4273d0de3f5a848a9652b64eddc9645
Lang Ming <langming.qzl@alibaba-inc.com>
2021-02-15 23:03:25 +0800

    fix: rewrite

--- rewrite/typescript.js
+++ rewrite/typescript.js
@@ -5,7 +5,7 @@ Module.prototype.require = function() {
   if (arguments[0] === 'typescript') {
     // 使用本地的 typescript
     var local = require.resolve(arguments[0], { paths: [process.cwd()] })
-    return originalRequire.apply(this, local)
+    return originalRequire.apply(this, [local])
   }
   return originalRequire.apply(this, arguments)
 }
